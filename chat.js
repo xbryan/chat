e9f9110e08a663b19f9dce5963cced110c4d93c9
Bryan Jennings <xbryanj@gmail.com>
2015-10-04 09:40:00 -0700

    Separate chat logic to different file

--- chat.js
+++ chat.js
@@ -1,1 +1,12 @@
-console.log('test');
+// Emits a "connection" event to the server.
+var socket = io();
+$('.sender').submit(function(event) {
+  var message = {user: $('.username').val(), message: $('.message').val()};
+  console.log(JSON.stringify(message));
+  socket.emit('message', message);
+  $('.message').val('');
+
+  // Returning false nullifies default form behavior, which is good. 
+  // It's similar to using event.preventDefault().
+  return false; 
+});
