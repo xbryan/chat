7b7c5a740794c5f3e6129bb9b02f2cbef7ec9ca5
Bryan Jennings <xbryanj@gmail.com>
2015-10-04 09:03:36 -0700

    Add connection event

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
 });
 
 http.listen(3000, function() {
