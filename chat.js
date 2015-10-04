bce3a471380a78b3f66a284eb21e0a868709b4c0
Bryan Jennings <xbryanj@gmail.com>
2015-10-04 11:21:45 -0700

    User now gets their message prepended locally

--- chat.js
+++ chat.js
@@ -6,6 +6,7 @@ $('.sender').submit(function(event) {
   console.log(JSON.stringify(message));
   socket.emit('message', message);
   $('.message').val('');
+  $('.messages').prepend(templateMessage(message));
 
   // Returning false nullifies default form behavior, which is good. 
   // It's similar to using event.preventDefault().
@@ -15,12 +16,14 @@ $('.sender').submit(function(event) {
 socket.on('message', function(message) {
   console.log('received message');
   console.log(JSON.stringify(message));
-  $('.messages').prepend(
-    '<div class="messageContainer">' +
-      '<span class="messageUser">' + message.user +
-      '</span>: ' +
-      '<span class="messageContent">' + message.message +
-      '</span>' +
-    '</div>'
-  );
+  $('.messages').prepend(templateMessage(message));
 });
+
+var templateMessage = function(message) {
+  return '<div class="messageContainer">' +
+    '<span class="messageUser">' + message.user +
+    '</span>: ' +
+    '<span class="messageContent">' + message.message +
+    '</span>' +
+  '</div>';
+};
